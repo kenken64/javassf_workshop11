c9ea1e9259a06cace90393e3d631b351815dc8ed
KENNETH PHANG <kennethphang@KENNETHs-MacBook-Air.local>
2022-01-05 14:18:08 +0800

    new

---
+++ src/main/java/sg/edu/nus/workshop11/Workshop11Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.workshop11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop11Application.class, args);
+	}
+
+}
